0d55f1b259ec0255177957693e529d2129695211
NourShkeir212 <n.ourshkeir@outlook.com>
2025-08-22 10:28:27 +0300

    Camera Bounds

--- lib/widgets/custom_google_maps.dart
+++ lib/widgets/custom_google_maps.dart
@@ -11,12 +11,31 @@ class CustomGoogleMaps extends StatefulWidget {
 class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
 
   late CameraPosition initialCameraPosition;
-  void initState(){
-    initialCameraPosition =CameraPosition(target: LatLng(33.51460461981359, 36.27705466419842));
+
+  void initState() {
+    initialCameraPosition = CameraPosition(
+        zoom: 13, target: LatLng(33.48664680612372, 36.30252545175364,
+    )
+    );
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(initialCameraPosition: initialCameraPosition);
+    return GoogleMap(
+        initialCameraPosition: initialCameraPosition,
+        cameraTargetBounds: CameraTargetBounds(
+          LatLngBounds(
+            southwest: LatLng(33.45, 36.2),
+            northeast: LatLng(33.55, 36.35),
+          ),
+        )
+    );
   }
 }
+
+// world view 0 -> 3
+// country view 4 -> 6
+// city view 10 -> 12
+// street view 13 -> 17
+// building view 18 -> 20
